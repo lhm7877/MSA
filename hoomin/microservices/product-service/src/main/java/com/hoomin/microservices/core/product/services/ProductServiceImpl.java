79ab2b0a67c577050a79125270460e1e1423f80a
lhm7877 <lhm7877@gmail.com>
2021-02-21 14:40:14 +0900

    feat(Productserviceimpl): 예외처리 추가

--- hoomin/microservices/product-service/src/main/java/com/hoomin/microservices/core/product/services/ProductServiceImpl.java
+++ hoomin/microservices/product-service/src/main/java/com/hoomin/microservices/core/product/services/ProductServiceImpl.java
@@ -2,6 +2,8 @@ package com.hoomin.microservices.core.product.services;
 
 import com.hoomin.api.core.product.Product;
 import com.hoomin.api.core.product.ProductService;
+import com.hoomin.util.exceptions.InvalidInputException;
+import com.hoomin.util.exceptions.NotFoundException;
 import com.hoomin.util.http.ServiceUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,6 +23,10 @@ public class ProductServiceImpl implements ProductService {
     @Override
     public Product getProduct(int productId) {
         LOG.debug("/product return the found product for productId={}", productId);
+
+        if (productId < 1) throw new InvalidInputException("Invalid productId: " + productId);
+
+        if (productId == 13) throw new NotFoundException("No product found for productId: " + productId);
 
         return new Product(productId, "name-" + productId, 123, serviceUtil.getServiceAddress());
     }
