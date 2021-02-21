1bc793cbd05f15a85e4547a28d41a3ba3508e274
lhm7877 <lhm7877@gmail.com>
2021-02-21 14:36:36 +0900

    feat(ProductServiceImpl): debug용 로그 추가

--- hoomin/microservices/product-service/src/main/java/com/hoomin/microservices/core/product/services/ProductServiceImpl.java
+++ hoomin/microservices/product-service/src/main/java/com/hoomin/microservices/core/product/services/ProductServiceImpl.java
@@ -3,10 +3,14 @@ package com.hoomin.microservices.core.product.services;
 import com.hoomin.api.core.product.Product;
 import com.hoomin.api.core.product.ProductService;
 import com.hoomin.util.http.ServiceUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ProductServiceImpl implements ProductService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ProductServiceImpl.class);
 
     private final ServiceUtil serviceUtil;
 
@@ -16,6 +20,8 @@ public class ProductServiceImpl implements ProductService {
 
     @Override
     public Product getProduct(int productId) {
+        LOG.debug("/product return the found product for productId={}", productId);
+
         return new Product(productId, "name-" + productId, 123, serviceUtil.getServiceAddress());
     }
 }
