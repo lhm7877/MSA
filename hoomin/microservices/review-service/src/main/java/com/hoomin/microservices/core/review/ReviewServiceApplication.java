014f253ea072050c3c8376ab12e9529f9eb523c6
lhm7877 <lhm7877@gmail.com>
2021-02-21 14:15:24 +0900

    build: 메인 클래스에 "com.hoomin" ComponentScan 추가
    api,util 모듈 검색하도록 기본 패키지 추가

--- hoomin/microservices/review-service/src/main/java/com/hoomin/microservices/core/review/ReviewServiceApplication.java
+++ hoomin/microservices/review-service/src/main/java/com/hoomin/microservices/core/review/ReviewServiceApplication.java
@@ -2,8 +2,10 @@ package com.hoomin.microservices.core.review;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.hoomin")
 public class ReviewServiceApplication {
 
     public static void main(String[] args) {
