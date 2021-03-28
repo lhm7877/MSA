4b2cd9385fbbc45ae1140b9ffdfec4d2bb1a8b24
ljw0829 <olo_jinwon@naver.com>
2021-03-28 20:16:18 +0900

    - feat : msa 스터디 커피집 운영  * 기본적인 모듈 추가 및 인터페이스 구성

---
+++ jinwon/coffee/microservice/order/src/main/java/com/msa/template/coffee/order/OrderApplication.java
@@ -0,0 +1,13 @@
+package com.msa.template.coffee.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(orderApplication.class, args);
+    }
+
+}
