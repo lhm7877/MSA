caf5b871d52c6990ad1610e43a8ccf6ed2e07263
srw1126 <srw1126@midasin.com>
2021-05-10 23:19:27 +0900

    refactor: api mapping 정보 중복 오류 수정

--- coffee/microservices/review-service/src/main/java/com/msa/template/coffee/review/controller/ReviewController.java
+++ coffee/microservices/review-service/src/main/java/com/msa/template/coffee/review/controller/ReviewController.java
@@ -1,20 +1,20 @@
 package com.msa.template.coffee.review.controller;
 
-import com.msa.template.coffee.api.core.review.dto.ReviewDto;
-import com.msa.template.coffee.api.core.review.service.ReviewService;
-import com.msa.template.coffee.review.entity.ReviewEntity;
-import com.msa.template.coffee.review.exception.InvalidInputException;
-import com.msa.template.coffee.review.repository.ReviewRepository;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Flux;
-import reactor.core.scheduler.Scheduler;
+import static java.util.stream.Collectors.*;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
-import static java.util.stream.Collectors.toList;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.msa.template.coffee.api.core.review.dto.ReviewDto;
+import com.msa.template.coffee.api.core.review.service.ReviewService;
+import com.msa.template.coffee.review.exception.InvalidInputException;
+import com.msa.template.coffee.review.repository.ReviewRepository;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Scheduler;
 
 @Slf4j
 @RestController
@@ -31,24 +31,6 @@ public class ReviewController implements ReviewService{
         if( productId < 1)
             throw new InvalidInputException("Invalid productId : " + productId);
         return asyncFlux(getByProductId(productId));
-    }
-
-    @Override
-    public Flux<ReviewDto> getReviewsByUserId(int userId) {
-        if( userId < 1)
-            throw new InvalidInputException("Invalid userId : " + userId);
-
-        return asyncFlux(getByUserId(userId));
-    }
-
-
-    private List<ReviewDto> getByUserId(long userId) {
-        List<ReviewDto> reviewDtoList = reviewRepository.findAllByUserId(userId).stream()
-                .map(e -> new ReviewDto(e.getUserId(), e.getProductId(), e.getContents(),e.getRating(), e.getUpdatedDate()))
-                .collect(toList());
-
-        log.debug("getByUserId : response size : {}", reviewDtoList.size());
-        return reviewDtoList;
     }
 
     protected List<ReviewDto> getByProductId(long productId) {
