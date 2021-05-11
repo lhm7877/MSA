d847d485ddc95d9035448c6df4d3870efcb36861
박정호 <pjh0904@midasin.com>
2021-05-11 10:27:18 +0900

    :art: 쿠폰 정의 변경

--- coffee/microservices/coupon-service/src/main/java/com/msa/template/coffee/couponservice/entity/CouponEntity.java
+++ coffee/microservices/coupon-service/src/main/java/com/msa/template/coffee/couponservice/entity/CouponEntity.java
@@ -1,34 +1,34 @@
 package com.msa.template.coffee.couponservice.entity;
 
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import javax.persistence.Column;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.Id;
+import javax.persistence.Version;
 
+// XXX 박정호 : 이 쿠폰은 기프티콘과 같이 중복이 없고, 고유한 쿠폰번호를 가지며, 메뉴 공짜 쿠폰임
+// XXX 박정호 : 멤버와 쿠폰은 다대일 관계
 @Document(collection = "coupon")
 public class CouponEntity {
 
-    @Id @Column(name = "coupon_id")
-    private Long id;
+    @Id
+    private String id;
 
-    private String name;
+    @Version
+    private Integer version;
 
-    private String number;
+    @Indexed(unique = true)
+    private int couponId;
 
-    private int count;
-
-    private int percent;
-
-    private int amount;
-
-    private int minPrice;
-
-    private int limitPrice;
-
-    private Long memberId;
+    private String productId;       // 제품 id
+    private String productName;     // 제품 명 (ex. 아이스아메리카노)
+    private int productPrice;       // 할인금액
+    private String couponNumber;    // 쿠폰번호 (ex. 3154-1355-1832-1234)
+    private Long memberId;          // 쿠폰 사용자
 
     @Enumerated(EnumType.STRING)
-    private CouponStatus couponStatus;
+    private CouponStatus couponStatus;  // 쿠폰상태(사용, 미사용, 만료)
 }
