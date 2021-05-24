31a6f4e5b240827d65b7eae59370d523b0947b9d
jeongho <qkrjh0904@gmail.com>
2021-05-25 00:27:05 +0900

    :art: MongoDB Test코드 작성 및 Test결과

--- coffee/microservices/coupon-service/src/main/java/com/msa/template/coffee/microservices/core/coupon/repository/CouponRepository.java
+++ coffee/microservices/coupon-service/src/main/java/com/msa/template/coffee/microservices/core/coupon/repository/CouponRepository.java
@@ -1,11 +1,10 @@
 package com.msa.template.coffee.microservices.core.coupon.repository;
 
 import com.msa.template.coffee.microservices.core.coupon.entity.CouponEntity;
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
+import org.springframework.data.repository.CrudRepository;
 
-public interface CouponRepository extends ReactiveMongoRepository<CouponEntity, String> {
-    Mono<CouponEntity> findByCouponId(int couponId);
-    Flux<CouponEntity> findByMemberId(Long memberId);
+import java.util.List;
+
+public interface CouponRepository extends CrudRepository<CouponEntity, String> {
+    List<CouponEntity> findByMemberId(Long memberId);
 }
--- coffee/microservices/coupon-service/src/main/java/com/msa/template/coffee/microservices/core/coupon/service/CouponServiceImpl.java
+++ coffee/microservices/coupon-service/src/main/java/com/msa/template/coffee/microservices/core/coupon/service/CouponServiceImpl.java
@@ -1,18 +1,6 @@
 package com.msa.template.coffee.microservices.core.coupon.service;
 
-import com.msa.template.coffee.api.core.coupon.dto.CouponDto;
-import com.msa.template.coffee.api.core.coupon.service.CouponService;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-public class CouponServiceImpl implements CouponService {
-    @Override
-    public Flux<CouponDto> getList(int memberId) {
-        return null;
-    }
-
-    @Override
-    public Mono<Boolean> use(CouponDto rq) {
-        return null;
-    }
+public class CouponServiceImpl{
 }
