4befff71f59efc6e5917215c0c5732c72a01f77a
ldg0831 <ldg0831@midasin.com>
2021-05-25 10:23:42 +0900

    [MODIFY] MemberRepository MongoDB 방식으로 변경

--- coffee/microservices/member-service/src/main/java/com/msa/template/coffee/memberservice/repository/MemberRepository.java
+++ coffee/microservices/member-service/src/main/java/com/msa/template/coffee/memberservice/repository/MemberRepository.java
@@ -1,16 +1,11 @@
 package com.msa.template.coffee.memberservice.repository;
 
-import com.msa.template.coffee.api.core.member.dto.MemberDto;
 import com.msa.template.coffee.memberservice.model.Member;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Mono;
 
-import java.util.Optional;
-
 @Repository
-public interface MemberRepository extends ReactiveCrudRepository<Member, String> {
-    Optional<Mono<Member>> findByUsername(String username);
-    
+public interface MemberRepository extends ReactiveMongoRepository<Member, String> {
     Mono<Member> findByMemberId(Integer memberId);
 }
